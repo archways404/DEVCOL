1121544b382de266d7cd4b4e8cdf2e049b7ced5b
archways404 <archways@gmx.us>
2024-04-02 15:52:39 +0200

    bugfix

--- api/core.js
+++ api/core.js
@@ -30,13 +30,15 @@ async function userExist(github_userid) {
 		await client.connect();
 		console.log('Connected to the database');
 		const database = client.db('userData');
-		const userid = database.collection('userid-github');
+		const useridCollection = database.collection('useridGithub');
 		const query = { userid: github_userid };
-		const test = await userid.findOne(query);
-		console.log(test);
+		const user = await useridCollection.findOne(query);
+		console.log(user);
+		return user; // Return the found user or null
+	} catch (error) {
+		console.error('Error in userExist:', error);
 	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
+		// Consider whether you need to close the connection here
 	}
 }
 
@@ -45,11 +47,17 @@ async function createUser(github_userid) {
 		await client.connect();
 		console.log('Connected to the database');
 		const database = client.db('userData');
-		const userid = database.collection('userid-github');
+		const userid = database.collection('useridGithub');
 		const newUser = { userid: github_userid };
 		const result = await userid.insertOne(newUser);
 		console.log(
 			`New listing created with the following id: ${result.insertedId}`
+		);
+		const userCollection = database.collection('userCollection');
+		const newCollection = { userid: github_userid };
+		const response = await userCollection.insertOne(newCollection);
+		console.log(
+			`New listing created with the following id: ${response.insertedId}`
 		);
 	} finally {
 		// Ensures that the client will close when you finish/error
@@ -108,16 +116,12 @@ app.get(
 	passport.authenticate('github', { failureRedirect: '/login' }),
 	async function (req, res) {
 		// Successful authentication, redirect home.
-		console.log(req.user);
-		const testing = await userExist(req.user.id).catch(console.dir);
-		console.log(req.user.id);
-		if (testing == null) {
-			console.log('User does not exist');
-			console.log(testing);
+		const user = await userExist(req.user.id);
+		if (!user) {
+			console.log('User does not exist, creating user');
 			await createUser(req.user.id);
 		} else {
 			console.log('User exists');
-			console.log(testing);
 		}
 		res.redirect('/');
 	}
