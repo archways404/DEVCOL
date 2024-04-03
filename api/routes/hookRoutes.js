2f9577928f1a6e39f4c78ed78db0fc8efaeee217
archways404 <archways@gmx.us>
2024-04-03 14:20:56 +0200

    test

--- api/routes/hookRoutes.js
+++ api/routes/hookRoutes.js
@@ -15,11 +15,13 @@ router.post('/webhook', (req, res) => {
 		return res.status(401).send('Mismatched signatures');
 	}
 
-  const ref = req.body.ref; // The branch that was pushed to
-	const isMerged = req.body.pull_request && req.body.pull_request.merged;
+	const ref = req.body.ref; // The branch that was pushed to
+	const isMergeCommit =
+		req.body.head_commit &&
+		req.body.head_commit.message.startsWith('Merge pull request');
 
-	if (ref === 'refs/heads/main' && isMerged) {
-		exec(`git pull && npm install`, (error, stdout, stderr) => {
+	if (ref === 'refs/heads/main' && isMergeCommit) {
+		exec(`git pull && npm install && npm run dev`, (error, stdout, stderr) => {
 			if (error) {
 				console.error(`exec error: ${error}`);
 				return res.status(500).send('Server Error');
